d0bbc19b45a10f73e33c74cefb520027184d6e7e
Hamit Okuyucular <111137936+hamitokuyucular@users.noreply.github.com>
2025-10-27 05:21:56 +0300

    Add files via upload

---
+++ toDoList.js
@@ -0,0 +1,54 @@
+const taskDOM = document.querySelector("#task")
+const listDOM = document.querySelector("#list")
+
+liveToastBtn.addEventListener("click", newElement)
+
+// 🔹 Sayfa açıldığında görevleri yükle
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTasks = localStorage.getItem("tasks");
+  if (savedTasks) {
+    listDOM.innerHTML = savedTasks;
+
+    // eventleri geri ekle
+    listDOM.querySelectorAll("li").forEach(li => {
+      checkFunction(li);
+      removeElement(li);
+    });
+  }
+});
+
+function newElement() {
+  if (taskDOM.value.trim() !== "") {
+    const liDOM = document.createElement("li");
+    liDOM.innerHTML = `${taskDOM.value}<span class="deleteItem">&times</span>`;
+    listDOM.append(liDOM);
+    taskDOM.value = "";
+    
+    checkFunction(liDOM);
+    removeElement(liDOM);
+
+    localStorage.setItem("tasks", listDOM.innerHTML);
+
+    $("#liveToast-s").toast("show");
+  } else {
+    $("#liveToast-e").toast("show");
+  }
+}
+
+
+function removeElement(element) {
+  element.querySelector('.deleteItem').addEventListener("click", function () {
+    element.remove();
+
+    localStorage.setItem("tasks", listDOM.innerHTML)
+  });
+}
+
+function checkFunction(item) {
+  item.addEventListener("click", () => {
+    item.classList.toggle("checked");
+
+    localStorage.setItem("tasks", listDOM.innerHTML);
+  });
+}
+
